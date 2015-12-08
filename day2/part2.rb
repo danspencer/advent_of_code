511b5277f46c7962a60a504488ae3501d784549b
Dan Spencer <Dan.Spencer@greatersudbury.ca>
2015-12-08 11:22:37 -0500

    Initial creation of the part 2 solution of day 2.

--- day2/part2.rb
+++ day2/part2.rb
@@ -9,3 +9,16 @@
 # A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
 # A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
 # How many total feet of ribbon should they order?
+
+dimensions = File.read("input.txt")
+
+total_feet = 0
+
+dimensions.each_line do |gift|
+  dimension = gift.split("x").map{|size| size.to_i}
+  smallest_sides = dimension.min 2
+  gift_size = smallest_sides[0] * 2 + smallest_sides[1] * 2 + dimension[0] * dimension[1] * dimension[2]
+  total_feet += gift_size
+end
+
+puts "The elves need #{total_feet} feet of ribbon."
