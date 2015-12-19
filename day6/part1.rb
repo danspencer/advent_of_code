7ba835490d53ad0c20ffe35a85dc7ef7281ea89a
Dan Spencer <da_spencer@hotmail.com>
2015-12-18 20:07:49 -0500

    Implemented part 1 solution of day 6.

--- day6/part1.rb
+++ day6/part1.rb
@@ -12,3 +12,37 @@
 # toggle 0,0 through 999,0 would toggle the first line of 1000 lights, turning off the ones that were on, and turning on the ones that were off.
 # turn off 499,499 through 500,500 would turn off (or leave off) the middle four lights.
 # After following the instructions, how many lights are lit?
+
+command = ""
+
+@lights = Array.new(1000) { Array.new(1000, false) }
+
+def parse_string(input)
+  command = input.match(/[[:alpha:]\s]*/).to_s.rstrip
+  starting_coordinate = input.match(/\d{1,3}[,]\d{1,3}/)
+  ending_coordinate = input.match(/\d{1,3}[,]\d{1,3}$/)
+  run_command(command, starting_coordinate, ending_coordinate)
+end
+
+def run_command(command, starting_coordinate, ending_coordinate)
+  sc = starting_coordinate.to_s.split(',')
+  ec = ending_coordinate.to_s.split(',')
+  (sc[0].to_i..ec[0].to_i).each do |row|
+    (sc[1].to_i..ec[1].to_i).each do |column|
+      if command == "turn on"
+        @lights[row][column] = true
+      elsif command == "turn off"
+        @lights[row][column] = false
+      else
+        @lights[row][column] = !@lights[row][column]
+      end
+    end
+  end
+end
+inputs = File.read("input.txt")
+
+inputs.each_line do |input|
+  parse_string(input)
+end
+
+puts @lights.flatten(1).count(true)
