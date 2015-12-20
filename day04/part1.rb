bef3761e28617ed81e462f37fd65726467164704
Dan Spencer <da_spencer@hotmail.com>
2015-12-20 16:23:08 -0500

    Implemented part 1 of day 4.

--- day04/part1.rb
+++ day04/part1.rb
@@ -9,3 +9,16 @@
 # If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043 starts with five zeroes (000001dbbfa...), and it is the lowest such number to do so.
 # If your secret key is pqrstuv, the lowest number it combines with to make an MD5 hash starting with five zeroes is 1048970; that is, the MD5 hash of pqrstuv1048970 looks like 000006136ef....
 # Your puzzle input is ckczppom.
+
+require 'digest'
+
+digest = Digest::MD5.new
+
+input = "ckczppom"
+count = 0
+
+while (!digest.hexdigest(input + count.to_s).match(/^00000/))
+  count = count + 1
+end
+
+puts "Count: #{count}"
