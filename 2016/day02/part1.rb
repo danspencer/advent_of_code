8bd78e759ff51b711fcbdd5772eb7ce8d2d73f22
Dan Spencer <da_spencer@hotmail.com>
2016-12-19 18:57:38 -0500

    Cleaned/simplified script. Implemented keypad so manual interpretation of current_location not needed.

--- 2016/day02/part1.rb
+++ 2016/day02/part1.rb
@@ -1,52 +1,52 @@
-require "pry"
+#!/usr/bin/env ruby
 
 input = File.read("input.txt")
 
+keypad = { '1': { x: -1, y: -1 },
+            '2': { x: 0, y: -1 },
+            '3': { x: 1, y: -1 },
+            '4': { x: -1, y: 0 },
+            '5': { x: 0, y: 0 },
+            '6': { x: 1, y: 0 },
+            '7': { x: -1, y: 1 },
+            '8': { x: 0, y: 1 },
+            '9': { x: 1, y: 1 } }
+
 x = 0
 y = 0
+bathroom_code = ""
 
-@current_location = { x: x, y: y }
-@orientation = { U: -1, R: 1, D: 1, L: -1 }
-
-def validate_next_location(direction)
-  #binding.pry
-  if direction == "L"
-    if @current_location["x".to_sym] + @orientation[direction.to_sym] > -2
-      @current_location = { x: @current_location["x".to_sym] + @orientation[direction.to_sym], y: @current_location[:y]}
-      return true
-    end
-  elsif direction == "D"
-    if @current_location["y".to_sym] + @orientation[direction.to_sym] < 2
-      @current_location = { x: @current_location[:x], y: @current_location["y".to_sym] + @orientation[direction.to_sym]}
-      return true
-    end
-  elsif direction == "R"
-    if @current_location["x".to_sym] + @orientation[direction.to_sym] < 2
-      @current_location = { x: @current_location["x".to_sym] + @orientation[direction.to_sym], y: @current_location[:y]}
-      return true
-    end
-  elsif direction == "U"
-    if @current_location["y".to_sym] + @orientation[direction.to_sym] > -2
-      @current_location = {x: @current_location[:x], y: @current_location["y".to_sym] + @orientation[direction.to_sym]}
-      return true
-    end
-  else
-    throw StandardError.new
-  end
-
-  return false
-end
+current_location = { x: x, y: y }
+orientation = { U: -1, R: 1, D: 1, L: -1 }
 
 input.each_line do |line|
   line.chomp.each_char do |direction|
-    if validate_next_location(direction)
-      #puts "Direction: #{direction} New location: #{@current_location}"
+    if direction == "L"
+      if current_location[:x] + orientation[direction.to_sym] > -2
+        current_location = { x: current_location[:x] + orientation[direction.to_sym], y: current_location[:y]}
+      end
+    elsif direction == "D"
+      if current_location[:y] + orientation[direction.to_sym] < 2
+        current_location = { x: current_location[:x], y: current_location[:y] + orientation[direction.to_sym]}
+      end
+    elsif direction == "R"
+      if current_location[:x] + orientation[direction.to_sym] < 2
+        current_location = { x: current_location[:x] + orientation[direction.to_sym], y: current_location[:y]}
+      end
+    elsif direction == "U"
+      if current_location[:y] + orientation[direction.to_sym] > -2
+        current_location = {x: current_location[:x], y: current_location[:y] + orientation[direction.to_sym]}
+      end
     else
-      #puts "Direction: #{direction} Instruction skipped"
+      puts "Invalid instruction: #{direction}"
     end
   end
-  puts "Final line location: #{@current_location}"
-  #binding.pry
+
+  keypad.each do |k,v|
+    if v[:x]==current_location[:x] && v[:y]==current_location[:y]
+      bathroom_code << k.to_s
+    end
+  end
 end
 
-#16138
+puts "The bathroom code is #{bathroom_code}."
